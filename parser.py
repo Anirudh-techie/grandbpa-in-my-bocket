e7ac2249ecaf9ee4e4b987deb95032e0f32a5c1f
Anirudh <anirudhmanojcv@gmail.com>
2024-07-29 13:08:37 +1000

    data parsing start

--- parser.py
+++ parser.py
@@ -1,8 +1,18 @@
-
+import json
 
 def get_scene_data():
+    with open("./data.json") as f:
+         data = json.load(f)
+         print(data)
     pass
 
 
-def get_dialogue_data():
+def get_dialogue_data(scene_index):
+    with open("./data.json") as f:
+            data = json.load(f)
+            scenes = data["scenes"]
     pass
+
+get_dialogue_data()
+
+
