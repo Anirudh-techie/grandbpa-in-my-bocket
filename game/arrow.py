bfa7a04b53e23652bb73d091d21c77658fed2af1
max <maxjrobbs@gmail.com>
2024-08-06 15:04:23 +1000

    modified arrow.py and added arrow images

--- game/arrow.py
+++ game/arrow.py
@@ -1,11 +1,17 @@
-import pygame
+import pygame as pg
 class Arrow:
-   def __init__(self,speed,x, icon:str) -> None:
+   def __init__(self,speed) -> None:
       self.speed = speed
-      self.x = x
-      self.y = 600
-      self.icon = icon
-      self.font = pygame.font.Font(None, 40)
+      self.up_x 
+      self.down_x
+      self.right_x
+      self.left_x
+
+      self.up_arrow = pg.image.load("res/game/arrows/up_arrow.png")
+      self.down_arrow = pg.image.load("res/game/arrows/down_arrow.png")
+      self.left_arrow = pg.image.load("res/game/arrows/left_arrow.png")
+      self.right_arrow = pg.image.load("res/game/arrows/right_arrow.png")
+      left_rect = pg.rect.Rect()
 
    def render(self, screen):
       text = self.font.render(self.icon, True, (0,0,0))
